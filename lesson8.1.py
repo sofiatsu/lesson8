6797e111c24b2bbb4eb97486fdc1172db4811825
sofiatsu <rfjdghjgh@gmail.com>
2025-01-29 20:11:38 +0100

    ДЗ 8.1.

--- lesson8.1.py
+++ lesson8.1.py
@@ -8,6 +8,7 @@ def add_one(some_list):
         my_lst.append(int(lst))
     return my_lst
 
+
 assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
 assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
 assert add_one([0]) == [1], 'Test3'
